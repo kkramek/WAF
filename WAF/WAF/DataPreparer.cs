20061cfe0e073f4f8b35748c4d59f26537d7890f
kkramek <kramekk@gmail.com>
2018-06-19 16:58:46 +0200

    Init

---
+++ WAF/WAF/DataPreparer.cs
@@ -0,0 +1,107 @@
+﻿using DataAccess;
+using libsvm;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace WAF
+{
+    class DataPreparer
+    {
+        private string negativeDataSetPath = @"C:\Users\kramek\Desktop\AIC#\NegativeDataSet.txt";
+        private string positiveDataSetPath = @"C:\Users\kramek\Desktop\AIC#\PositiveDataSet.txt";
+
+        private List<string> requestText;
+        private double[] classValue;
+        private List<string> vocabulary;
+
+        public List<string> RequestText
+        {
+            get { return requestText; }
+            set { requestText = value; }
+        }
+        public double[] ClassValue
+        {
+            get { return classValue; }
+            set { classValue = value; }
+        }
+
+        public List<string> Vocabulary
+        {
+            get { return vocabulary; }
+            set { vocabulary = value; }
+        }
+
+        public DataPreparer()
+        {
+            int negativeRowNumber;
+            int positiveRowNumber;
+
+            this.requestText = ReadDataSetFromFile(this.negativeDataSetPath).ToList();
+            negativeRowNumber = this.requestText.Count;
+            this.requestText.AddRange(ReadDataSetFromFile(this.positiveDataSetPath));
+            positiveRowNumber = this.requestText.Count - negativeRowNumber;
+
+            SetClassValues(negativeRowNumber, positiveRowNumber);
+
+            this.vocabulary = requestText.SelectMany(GetWords).Distinct().OrderBy(word => word).ToList();
+
+        }
+
+        public void SetClassValues(int negativeRowNumber, int positiveRowNumber)
+        {
+            this.classValue = new double[negativeRowNumber + positiveRowNumber];
+
+
+            for (int i = 0; i < negativeRowNumber + 1; ++i)
+            {
+                this.classValue[i] = -1;
+            }
+
+            for (int i = negativeRowNumber+1; i < negativeRowNumber + positiveRowNumber; ++i)
+            {
+                this.classValue[i] = 1;
+            }
+
+        }
+
+        public IEnumerable<String> ReadDataSetFromFile(string path)
+        {
+            return File.ReadLines(path);
+        }
+
+        private static IEnumerable<string> GetWords(string requestText)
+        {
+            string result = requestText; //SeparateNonAlphanumeric(requestText);//
+
+            return result.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private static string SeparateNonAlphanumeric(string line)
+        {
+            string result = "";
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (Char.IsLetter(line[i]) || Char.IsNumber(line[i]))
+                {
+                    result += line[i];
+                }
+                else
+                {
+                    result += ' ';
+                    result += line[i];
+                    result += ' ';
+                }
+
+            }
+
+            return result;
+        }
+
+    }
+
+
+}
---
+++ WAF/WAF/Program.cs
@@ -0,0 +1,76 @@
+﻿using DataAccess;
+using libsvm;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WAF
+{
+    class Program
+    {
+        private static Dictionary<int, string> _predictionDictionary = new Dictionary<int, string> { { -1, "SqlInjection" }, { 1, "SafeRequest" } };
+
+        static void Main(string[] args)
+        {
+
+            DataPreparer data = new DataPreparer();
+
+            var problemBuilder = new TextClassificationProblemBuilder();
+            var problem = problemBuilder.CreateProblem(data.RequestText, data.ClassValue, data.Vocabulary.ToList());
+
+            const double C = 0.5;
+            var model = new C_SVC(problem, KernelHelper.LinearKernel(), C); // Train is called automatically here
+            var accuracy = model.GetCrossValidationAccuracy(100);
+
+            Console.Clear();
+            Console.WriteLine(new string('=', 50));
+            Console.WriteLine("Accuracy of the model is {0:P}", accuracy);
+
+            model.Export(string.Format(@"C:\Users\kramek\Desktop\AIC#\model_{0}_accuracy.model", accuracy));
+
+            Console.WriteLine(new string('=', 50));
+            Console.WriteLine("The Model is ready. \r\nEnter a request to check:");
+            Console.WriteLine(new string('=', 50));
+
+            string userInput;
+
+            do
+            {
+                userInput = Console.ReadLine(); // SeparateNonAlphanumeric(Console.ReadLine());//
+                var newX = TextClassificationProblemBuilder.CreateNode(userInput, data.Vocabulary);
+                //var predictedYProb = model.PredictProbabilities(newX);
+                var predictedY = model.Predict(newX);
+
+                Console.WriteLine("The prediction is {0}", _predictionDictionary[(int)predictedY]);
+                Console.WriteLine(new string('=', 50));
+
+            } while (userInput != "exit");
+
+            Console.WriteLine("");
+
+        }
+
+        private static string SeparateNonAlphanumeric(string line)
+        {
+            string result = "";
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (Char.IsLetter(line[i]) || Char.IsNumber(line[i]))
+                {
+                    result += line[i];
+                }
+                else
+                {
+                    result += ' ';
+                    result += line[i];
+                    result += ' ';
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
